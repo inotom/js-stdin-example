dd294b1cb2ea23232ac0ebf10e3d016f01f43081
inotom <wdf7322@yahoo.co.jp>
2017-09-15 09:41:19 +0900

    Fix max number to use parseInt

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ process.stdin.on('end', function() {
   var lines = input.trim().split('\n');
 
   if (lines.length > 0) {
-    var max = lines[0];
+    var max = parseInt(lines[0], 10);
+
     for (var i = 0; i < max; i++) {
       var n = i + 1;
+
       if ((n % 3 === 0) && (n % 5 === 0)) {
         console.log('Fizz Buzz');
       } else if (n % 3 === 0) {
